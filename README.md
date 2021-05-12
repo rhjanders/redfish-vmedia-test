f3bf7aa4debf17df63fde8895ca00f30e48bfdb0
Jacob Anders <janders@redhat.com>
2021-05-12 16:29:57 +1000

    Initial import.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # redfish-vmedia-test
+A simple tool which can validate virtual media attachment and detachment via Redfish using sushy library
+
+Usage:
+
+Dell R640:
+python vmedia-attach.py --username [username] --password [password] --bmc_url https://[bmc_ip]/redfish/v1 --manager_url v1/Managers/iDRAC.Embedded.1 --image_url [URL, e.g. https://example.com/my.iso]
+
+HP e910:
+python vmedia-attach.py --username [username] --password [password] --bmc_url https://[bmc_ip]/redfish/v1 --manager_url v1/Managers/1 --image_url [URL, e.g. https://example.com/my.iso]
+
+Supermicro 5039MS:
+python vmedia-attach.py --username [username] --password [password] --bmc_url https://[bmc_ip]/redfish/v1 --manager_url v1/Managers/1/VM1/CfgCD --image_url [URL, e.g. https://example.com/my.iso]
---
+++ vmedia-attach/vmedia-attach.py
@@ -0,0 +1,27 @@
+import sushy
+from sushy import auth
+from sushy import exceptions
+import urllib3
+urllib3.disable_warnings()
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--username")
+parser.add_argument("--password")
+parser.add_argument("--bmc_url")
+parser.add_argument("--manager_url")
+parser.add_argument("--image_url")
+args = parser.parse_args()
+
+basic_auth = auth.BasicAuth(username=args.username, password=args.password)
+s = sushy.Sushy(args.bmc_url, verify=False, auth=basic_auth)
+mgr_inst = s.get_manager(args.manager_url)
+vmedia_list = mgr_inst.virtual_media.get_members()
+
+for vmedia in vmedia_list:
+    if "dvd" in vmedia.media_types:
+        vmedia.eject_media()
+        vmedia.refresh()
+        print("vmedia state prior to attempting attach: media_types:", vmedia.media_types, "image:" , vmedia.image, "inserted:", vmedia.inserted)
+        vmedia.insert_media(args.image_url, inserted=True, write_protected=True)
+        vmedia.refresh()
+        print("vmedia state after attempting attach: media_types:", vmedia.media_types, "image:" , vmedia.image, ",inserted:", vmedia.inserted)
