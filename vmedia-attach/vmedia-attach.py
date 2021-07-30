d2c7b131616343fb0437d1574997fdabc1e83f76
Jacob Anders <janders@redhat.com>
2021-07-30 11:26:39 +1000

    Minor fixes to the script. Added a wait to avoid race condition.

--- vmedia-attach/vmedia-attach.py
+++ vmedia-attach/vmedia-attach.py
@@ -4,6 +4,7 @@ from sushy import exceptions
 import urllib3
 urllib3.disable_warnings()
 import argparse
+import time
 parser = argparse.ArgumentParser()
 parser.add_argument("--username")
 parser.add_argument("--password")
@@ -11,7 +12,6 @@ parser.add_argument("--bmc_url")
 parser.add_argument("--manager_url")
 parser.add_argument("--image_url")
 parser.add_argument("--eject")
-parser.add_argument("--type")
 args = parser.parse_args()
 
 basic_auth = auth.BasicAuth(username=args.username, password=args.password)
@@ -20,11 +20,12 @@ mgr_inst = s.get_manager(args.manager_url)
 vmedia_list = mgr_inst.virtual_media.get_members()
 
 for vmedia in vmedia_list:
-    if args.type in vmedia.media_types:
+    if "dvd" in vmedia.media_types:
         if args.eject:
             vmedia.eject_media()
         vmedia.refresh()
         print("vmedia state prior to attempting attach: media_types:", vmedia.media_types, "image:" , vmedia.image, "inserted:", vmedia.inserted)
         vmedia.insert_media(args.image_url, inserted=True, write_protected=True)
+        time.sleep(1)
         vmedia.refresh()
         print("vmedia state after attempting attach: media_types:", vmedia.media_types, "image:" , vmedia.image, ",inserted:", vmedia.inserted)
