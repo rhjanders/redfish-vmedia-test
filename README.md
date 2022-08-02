0656fd760ca8e4bcba3606698e600338e8d7c918
Jacob Anders <janders@redhat.com>
2022-08-02 21:59:35 +1000

    Added test script for boot sequence changes.

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ python vmedia-attach.py --username [username] --password [password] --bmc_url ht
 
 Supermicro 5039MS:
 python vmedia-attach.py --username [username] --password [password] --bmc_url https://[bmc_ip]/redfish/v1 --manager_url v1/Managers/1/VM1/CfgCD --image_url [URL, e.g. https://example.com/my.iso]
+
+# bootseq-update
+A simple tool which can validate boot order changes via Redfish using sushy library
+
+Usage:
+
+Dell R640:
+python change_bootseq.py --username [username] --password [password] --bmc_url https://[bmc_host] --system_url /redfish/v1/Systems/System.Embedded.1 --boot_device hdd
+
+HP DL360 G9
+python change_bootseq.py --username [username] --password [password] --bmc_url https://[bmc_host] --system_url /redfish/v1/Systems/1/ --boot_device hdd
+
---
+++ bootseq-update/change_bootseq.py
@@ -0,0 +1,30 @@
+import sushy
+from sushy import auth
+from sushy import exceptions
+import urllib3
+urllib3.disable_warnings()
+import argparse
+import sys
+import time
+parser = argparse.ArgumentParser()
+parser.add_argument("--username")
+parser.add_argument("--password")
+parser.add_argument("--bmc_url")
+parser.add_argument("--system_url")
+parser.add_argument("--boot_device")
+args = parser.parse_args()
+
+if args.boot_device == "pxe":
+    bootdev = sushy.BOOT_SOURCE_TARGET_PXE
+if args.boot_device == "hdd":
+    bootdev = sushy.BOOT_SOURCE_TARGET_HDD
+
+basic_auth = auth.BasicAuth(username=args.username, password=args.password)
+s = sushy.Sushy(args.bmc_url, verify=False, auth=basic_auth)
+
+system = s.get_system(args.system_url)
+try:
+    system.set_system_boot_options(bootdev, sushy.BOOT_SOURCE_ENABLED_ONCE)
+    print("Setting boot device succeeded")
+except sushy.exceptions.SushyError:
+    print("Setting boot device failed")
